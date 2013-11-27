e2983202c17f230f5eb2391840f3e41b8654e303
Néstor Álvarez Díaz <alu0100597943@ull.edu.es>
2013-11-27 11:44:45 +0000

    Path solucionados

--- lib/matriz_lpp.rb
+++ lib/matriz_lpp.rb
@@ -1,5 +1,7 @@
 require "matriz_lpp/version"
 
 module MatrizLpp
-  # Your code goes here...
+  require 'matriz_lpp/racional.rb'
+  require 'matriz_lpp/matriz.rb'
+  require 'matriz_lpp/dispersa.rb'
 end
